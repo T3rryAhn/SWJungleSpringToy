1b9814085b1fcda6ca31582acbd7a24d7cb354cb
t3rryahn <t3rryahn.dev@gmail.com>
2024-06-10 20:40:39 +0900

    refactor: 로그인 요청 dto 개선
    - 로그인 요청시 username과 password 에 올바른 패턴을 받았는지 체크 추가

--- src/main/java/swjungle/springboard/dto/LoginRequestDto.java
+++ src/main/java/swjungle/springboard/dto/LoginRequestDto.java
@@ -1,14 +1,21 @@
 package swjungle.springboard.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
 
-    @NotBlank
+    @NotBlank(message = "Username 을 입력하세요.")
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[a-z0-9]+$", message = "잘못된 Username 형식입니다.")
     private String userName;
 
-    @NotBlank
+    @NotBlank(message = "Password 를 입력하세요.")
+    @Size(min = 8, max = 15)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$"
+            , message = "잘못된 Password 형식입니다.")
     private String password;
 }
