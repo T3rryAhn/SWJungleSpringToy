063c480af1eb6ffb123672a25640fea4d1a62b65
t3rryahn <t3rryahn.dev@gmail.com>
2024-06-10 20:28:46 +0900

    refactor: 회원가입 요청 dto 수정.
    - password 에 반드시 알파벳 대소문자, 숫자, 특수문자를 포함하도록 패턴 추가.
    - 메시지 한글화

--- src/main/java/swjungle/springboard/dto/SignupRequestDto.java
+++ src/main/java/swjungle/springboard/dto/SignupRequestDto.java
@@ -7,13 +7,14 @@ import lombok.Getter;
 
 @Getter
 public class SignupRequestDto {
-    @NotBlank(message = "Username is mandatory")
+    @NotBlank(message = "Username 은 필수입니다.")
     @Size(min = 4, max = 10)
-    @Pattern(regexp = "^[a-z0-9]+$", message = "Username must be lowercase alphanumeric")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "Username 은 소문자 알파벳 또는 숫자로 구성되어야 합니다.")
     private String userName;
 
-    @NotBlank(message = "Password is mandatory")
+    @NotBlank(message = "Password 은 필수입니다.")
     @Size(min = 8, max = 15)
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password must be alphanumeric")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$"
+            , message = "Password 은 알파벳 대소문자, 숫자, 특수문자로 구성되어야 합니다.")
     private String password;
 }
